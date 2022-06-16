ef46ac04d3ec7f2ab564abb58766a18c28102425
Anton Trollbäck <anton@trollback.se>
2022-06-16 19:22:51 +0200

    Fix page force renderings

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -82,10 +82,10 @@
   </div>
 
   <div class="u-hidden">
-    <a href="https://climate-events.nytimes.com/thank-you">.</a>
-    <a href="https://climate-events.nytimes.com/climate-forward/press-thank-you">.</a>
-    <a href="https://climate-events.nytimes.com/climate-forward/london-thank-you">.</a>
-    <a href="https://climate-events.nytimes.com/climate-forward/london-video-test">.</a>
+    <a href="/thank-you">.</a>
+    <a href="/climate-forward/press-thank-you">.</a>
+    <a href="/climate-forward/london-thank-you">.</a>
+    <a href="/climate-forward/london-video-test">.</a>
   </div>
 </footer>
 
