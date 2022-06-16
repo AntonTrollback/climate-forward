e656486bd0b84b46b38e639ce931fc1d84a16f4a
Carl Törnqvist <carl@tornqv.ist>
2022-06-16 17:55:40 +0200

    Fix empty links

--- src/lib/Slices.svelte
+++ src/lib/Slices.svelte
@@ -196,7 +196,10 @@
     {/if}
 
     {#if slice.slice_type === 'sponsors'}
-      <Sponsors items={slice.items.filter((sponsor) => !sponsor.isBroken)} />
+      <Sponsors
+        items={slice.items.filter(
+          (sponsor) => sponsor.id && !sponsor.isBroken
+        )} />
     {/if}
 
     {#if slice.slice_type === 'button'}
@@ -285,7 +288,7 @@
           limit={slice.primary.limit}
           items={slice.items
             .map((item) => item.speaker)
-            .filter((speaker) => !speaker.isBroken)} />
+            .filter((speaker) => speaker.id && !speaker.isBroken)} />
       </div>
     {/if}
 
@@ -294,7 +297,7 @@
         <Program
           sessions={slice.items
             .map((item) => item.session)
-            .filter((session) => !session.isBroken)} />
+            .filter((session) => session.id && !session.isBroken)} />
       </div>
     {/if}
   </div>
