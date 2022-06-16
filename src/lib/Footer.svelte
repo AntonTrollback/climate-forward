bd0abd28199fb0d26c9f8e11fd17e032dd15a142
Anton Trollbäck <anton@trollback.se>
2022-06-16 19:30:31 +0200

    Fix video test link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -85,7 +85,7 @@
     <a href="/thank-you">.</a>
     <a href="/climate-forward/press-thank-you">.</a>
     <a href="/climate-forward/london-thank-you">.</a>
-    <a href="/climate-forward/london-video-test">.</a>
+    <a href="/climate-forward/events/london-video-test">.</a>
   </div>
 </footer>
 
