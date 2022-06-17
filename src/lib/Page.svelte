f493c1eb1fc6a3e1fc7b062dc27f49515bcf7458
Anton Trollbäck <anton@trollback.se>
2022-06-17 10:05:29 +0200

    Fix routing for page speakers

--- src/lib/Page.svelte
+++ src/lib/Page.svelte
@@ -1,9 +1,50 @@
 <script>
+  import { setContext } from 'svelte'
+  import { setMeta } from './Meta.svelte'
   import Slices from './Slices.svelte'
   import Menu from './Menu.svelte'
+  import Link from './Link.svelte'
+  import Speaker from './Speaker.svelte'
   import Footer from './Footer.svelte'
+  import Modal from './Modal.svelte'
+  import { LINK } from './Link.svelte'
+  import resolve from './utils/resolve.js'
   export let page
   export let parent = null
+  export let speaker = null
+
+  setContext(LINK, function (document) {
+    if (document === page) {
+      const href = resolve(document, parent)
+      return {
+        href: href,
+        'sveltekit:reload': '',
+        onclick(e) {
+          speaker = null
+          setMeta(document, parent)
+          window.history.replaceState({}, null, href)
+          e.preventDefault()
+        }
+      }
+    }
+
+    switch (document.type) {
+      case 'speaker': {
+        const href = resolve(document, [parent, page, '/speakers'])
+        return {
+          href: href,
+          'sveltekit:reload': '',
+          onclick(e) {
+            speaker = document
+            setMeta(speaker, page)
+            e.preventDefault()
+          }
+        }
+      }
+      default:
+        return { href: resolve(document) }
+    }
+  })
 </script>
 
 <Menu
@@ -15,6 +56,13 @@
   branding={page.data.branding || parent.data.branding} />
 
 <Slices slices={page.data.body} />
+
+{#if speaker}
+  <Modal>
+    <Speaker {speaker} />
+    <Link slot="close" document={page}>Close</Link>
+  </Modal>
+{/if}
 
 <Footer
   prefix={parent}
