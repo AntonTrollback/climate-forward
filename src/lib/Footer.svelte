3e99b8cfb553c6caf7ce3461667fe2a3a156bbe1
Anton Trollbäck <anton@trollback.se>
2022-06-17 08:40:34 +0200

    Add redirects

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -81,13 +81,14 @@
     <p>{@html copyright ? whitespace(copyright) : ''}</p>
   </div>
 
-  <div class="u-hidden">
+  <div class="u-hidden" aria-hidden="true">
     <a href="/thank-you">.</a>
     <a href="/climate-forward/press-thank-you">.</a>
     <a href="/climate-forward/london-thank-you">.</a>
     <a href="/climate-forward/events/london-video-test">.</a>
     <a href="/climate-forward/the-fashion-opportunity">.</a>
     <a href="/the-fashion-opportunity">.</a>
+    <a href="/404">.</a>
   </div>
 </footer>
 
