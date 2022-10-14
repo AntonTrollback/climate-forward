acf302759be6a35e9ec507f45f7d0cda2003c592
Anton Trollbäck <anton@trollback.se>
2022-10-14 10:27:02 +0200

    Fix 404 pages

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -6,10 +6,25 @@
 </script>
 
 <script>
-  export let status
+  import Menu from '../lib/Menu.svelte'
+  import Button from '../lib/Button.svelte'
 </script>
 
+<Menu slices={null} keeptop="true" branding={'Climate Events'} />
+
+<div class="u-container">
+  <div class="Text Text--spaced Text--xl">
+    <h3 class="Text-h3">Page Not Found</h3>
+    <p class="Text-p">
+      We’re sorry, we seem to have lost this page, but&nbsp;we don’t want to
+      lose you.
+    </p>
+  </div>
+</div>
+<div class="u-container">
+  <Button class="u-spaceXl" href="/">See all events</Button>
+</div>
+
 <svelte:head>
-  <title
-    >{status}, {status === 404 ? 'Not found' : 'Something went wrong'}</title>
+  <title>Page Not Found | The New York Times Climate Events</title>
 </svelte:head>
