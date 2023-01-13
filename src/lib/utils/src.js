52b76b4c40d093529bdbfabecaeff1957e1a7012
Anton Trollbäck <anton@trollback.se>
2023-01-13 16:33:23 +0100

    Fix two broken session images…

--- src/lib/utils/src.js
+++ src/lib/utils/src.js
@@ -4,6 +4,9 @@ export default function src(transforms, src) {
   src = src
     .replace('auto=compress,format&', '')
     .replace('?auto=compress,format', '')
-    .replace('hqdefault.jpg', 'maxresdefault.jpg')
+  if (!src.includes('8ICWinVj7X0') && !src.includes('bcndxkvXkAQ')) {
+    src = src.replace('hqdefault.jpg', 'maxresdefault.jpg')
+  }
+
   return `https://res.cloudinary.com/${CLOUDNAME}/image/fetch/${transforms}/${src}`
 }
