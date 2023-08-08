417fd63d2f6f9df613c224f137ea7eb4546a7f6d
Anton Trollbäck <anton@trollback.se>
2023-08-08 10:33:23 +0200

    Enable minification

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 
 const config = {
-  plugins: [sveltekit()],
-  build: {
-    minify: false
-  }
+  plugins: [sveltekit()]
 }
 
 export default config
